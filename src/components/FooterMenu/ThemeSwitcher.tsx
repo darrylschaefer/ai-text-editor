388e34f469f1983d6a0c6a6fa4b89e04308eda44
darrylschaefer <119073511+darrylschaefer@users.noreply.github.com>
2024-02-12 11:34:46 -0500

    Initial commit

---
+++ src/components/FooterMenu/ThemeSwitcher.tsx
@@ -0,0 +1,40 @@
+import { useEffect } from 'react';
+import { useTranslation } from 'react-i18next';
+import useStore from '@store/store';
+import { Sun, Moon } from '@carbon/icons-react';}
+import { Theme } from '@type/theme';
+
+const getOppositeTheme = (theme: Theme): Theme => {
+  if (theme === 'dark') {
+    return 'light';
+  } else {
+    return 'dark';
+  }
+};
+const ThemeSwitcher = () => {
+  const { t } = useTranslation();
+  const theme = useStore((state) => state.theme);
+  const setTheme = useStore((state) => state.setTheme);
+
+  const switchTheme = () => {
+    setTheme(getOppositeTheme(theme!));
+  };
+
+  useEffect(() => {
+    document.documentElement.className = theme;
+  }, [theme]);
+
+  return theme ? (
+    <button
+      className='items-center gap-3 btn btn-neutral'
+      onClick={switchTheme}
+    >
+      {theme === 'dark' ? <Sun /> : <Moon />}
+      {t(getOppositeTheme(theme) + 'Mode')}
+    </button>
+  ) : (
+    <></>
+  );
+};
+
+export default ThemeSwitcher;
