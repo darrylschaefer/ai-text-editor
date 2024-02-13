40743aa82c441c2c05f2ffcdfb0503c22865c2b8
darrylschaefer <119073511+darrylschaefer@users.noreply.github.com>
2024-02-13 10:23:45 -0500

    Initial commit

---
+++ src/components/style.tsx
@@ -0,0 +1,7 @@
+const defaultStyles = {
+    buttonNav: "flex py-2 px-2 items-center gap-3 hover:bg-gray-500/10 transition-colors duration-200 text-white text-sm flex-shrink-0 border border-transparent transition-opacity cursor-pointer opacity-100 snap-start",
+    buttonStyle: "flex py-2 px-2 items-center gap-3 hover:bg-gray-500/10 transition-colors duration-200 text-white text-sm flex-shrink-0 border border-transparent transition-opacity cursor-pointer opacity-100 snap-start",
+    buttonLongStyle: "flex flex-1 items-center hover:bg-gray-500/10 transition-all duration-200 text-white text-sm flex-shrink-0 cursor-pointer opacity-100 py-2 pr-2 pl-3 gap-3 border border-white/10 snap-start"
+}
+
+export default defaultStyles;
